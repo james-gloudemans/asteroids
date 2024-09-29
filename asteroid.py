3a8e79330bfec620edcc8ae5534269b1b93ae1d4
James Gloudemans <james.gloudemans@gmail.com>
2024-09-29 15:42:00 -0400

    add asteroid wrapping around screen edges

--- asteroid.py
+++ asteroid.py
@@ -10,6 +10,14 @@ class Asteroid(CircleShape):
         super().__init__(x, y, radius)
         
     def draw(self, screen: pygame.Surface) -> None:
+        if self.position.x > SCREEN_WIDTH + ASTEROID_MAX_RADIUS:
+            self.position.x = 0
+        if self.position.y > SCREEN_HEIGHT + ASTEROID_MAX_RADIUS:
+            self.position.y = 0
+        if self.position.x < -ASTEROID_MAX_RADIUS:
+            self.position.x = SCREEN_WIDTH
+        if self.position.y < -ASTEROID_MAX_RADIUS:
+            self.position.y = SCREEN_HEIGHT
         pygame.draw.circle(screen, (255,255,255), self.position, self.radius, 2)
         
     def update(self, dt: int) -> None:
--- main.py
+++ main.py
@@ -13,8 +13,14 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.font.init()
+    game_font = pygame.font.SysFont('Comic Sans MS', 30)
     clock = pygame.time.Clock()
-    dt = 0
+    dt: int = 0
+    score: int = 0
+    score_text = game_font.render(f'Score: {score}', False, (255,255,255))
+    lives = 3
+    lives_text = game_font.render(f'Extra Lives: {lives}', False, (255,255,255))
     
     updatables = pygame.sprite.Group()
     drawables = pygame.sprite.Group()
@@ -38,13 +44,25 @@ def main():
             obj.draw(screen)
         for asteroid in asteroids:
             if asteroid.collides_with(player):
-                print("Game over!")
-                exit()
+                lives -= 1
+                if lives <= 0:
+                    print("Game over!")
+                    exit()
+                else:
+                    lives_text = game_font.render(f'Extra Lives: {lives}', False, (255,255,255))
+                    for obj in drawables:
+                        obj.kill()
+                    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+                    field = AsteroidField()
             for shot in shots:
                 if asteroid.collides_with(shot):
+                    score += 10
+                    score_text = game_font.render(f'Score: {score}', False, (255,255,255))
                     asteroid.split()
                     shot.kill()
-                
+
+        screen.blit(score_text, (TEXT_PADDING,0))
+        screen.blit(lives_text, (SCREEN_WIDTH - (lives_text.get_width() + TEXT_PADDING), 0))
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
